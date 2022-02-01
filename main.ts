417b1a7ecd4edf39eaf446613bc452f511c9000f
Your Name <you@example.com>
2022-02-01 11:01:29 -0600

    add prep for in-app settings; fix typos

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, FuzzySuggestModal, SuggestModal } from 'obsidian';
 
-interface MyPluginSettings {
-	mySetting: string;
+interface GrabSettings {
+	yamlGrabbed: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	yamlGrabbed: 'uri'
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+	settings: GrabSettings;
+	console.log(this);
 	async onload() {
 		this.addCommand({
 			id: "open-files-url",
@@ -22,13 +22,15 @@ export default class MyPlugin extends Plugin {
 		});
 	}
 
+	//this.addSettingTab(new GrabSettingTab(this.app, this));
+
 	onunload() {
 
-	}
+	};
 }
 
 // https://github.com/ryanjamurphy/workbench-obsidian/blob/master/main.ts
-class workbenchNameModal extends FuzzySuggestModal<string> { // thanks to Licat for the assist!
+class workbenchNameModal extends FuzzySuggestModal<string> {
 	app: App;
 
     constructor(app: App) {
@@ -37,8 +39,8 @@ class workbenchNameModal extends FuzzySuggestModal<string> { // thanks to Licat 
     }
 
     getItems(): string[] {
-		let files = this.app.vault.getMarkdownFiles();
-		let fileList = files.map(file => file.name);
+		const files = this.app.vault.getMarkdownFiles();
+		const fileList = files.map(file => file.name);
         return fileList;
     }
 
@@ -49,7 +51,35 @@ class workbenchNameModal extends FuzzySuggestModal<string> { // thanks to Licat 
     async onChooseItem(item: string, evt: MouseEvent | KeyboardEvent): void {
 	const {getPropertyValue} = app.plugins.plugins["metaedit"].api;
 	//app.vault.getAbstractFileByPath(item)
-	const URL = await getPropertyValue("url", item);
+	const URL = await getPropertyValue("uri", item);
 	window.open(URL);
     }
+}
+class GrabSettingTab extends PluginSettingTab {
+	plugin: Grab;
+
+	constructor(app: App, plugin: Grab) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+
+		new Setting(containerEl)
+			.setName('Setting #1')
+			.setDesc('It\'s a secret')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue(this.plugin.settings.mySetting)
+				.onChange(async (value) => {
+					console.log('Secret: ' + value);
+					this.plugin.settings.mySetting = value;
+					await this.plugin.saveSettings();
+				}));
+	}
 }
