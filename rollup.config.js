14ec9a44df12bcb9a8159f1cc918ad4674da45b8
Your Name <you@example.com>
2022-02-01 10:58:30 -0600

    remove ESBuild; add rollup config w/ plugins

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import eslint from '@rollup/plugin-eslint';
+import copy from "rollup-plugin-copy";
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'main.ts',
+  output: {
+	dir: "test-vault/.obsidian/plugins/ObsidianMD-Quick-Grab/",
+        sourcemap: "inline",
+        format: "cjs",
+        exports: "default",
+  },
+  plugins: [
+    typescript(),
+    eslint({
+      /* your options */
+    }),
+    copy({
+    	targets: [
+		{ src: "manifest.json", dest: "test-vault/.obsidian/plugins/ObsidianMD-Quick-Grab/" },
+		{ src: "styles.css", dest: "test-vault/.obsidian/plugins/ObsidianMD-Quick-Grab/" }
+	]}),
+  ]
+};
